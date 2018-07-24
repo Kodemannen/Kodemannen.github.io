8cb3f7a9cbcb2cc26175a93acb0b2c5d7df08007
kodemannen <samuelknudsen@hotmail.com>
2018-07-24 13:19:18 +0200

    no message

---
+++ myjs.js
@@ -0,0 +1,20 @@
+// document.getElementById("penis").innerHTML = "asd!";
+
+// var randVar = Math.random();
+// if (randVar > 0.5) {
+//     document.getElementById("penis").innerHTML = "asd!";
+// };
+
+var dir = 'Comics'; // your directory
+
+
+
+document.getElementById("test").innerHTML = "huldra";
+
+
+// var filenames = "";
+
+// for (var i = 0; i<files.length; i++) {
+//     console.log(files[i]);
+// }
+
