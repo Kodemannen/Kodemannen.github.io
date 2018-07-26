002e1db52f73e456388bc5f8dec6ed61fde34af4
kodemannen <samuelknudsen@hotmail.com>
2018-07-26 15:34:40 +0200

    saldkj

--- myjs.js
+++ myjs.js
@@ -1,4 +1,4 @@
-var filenames = ['Einstein.png', 'Auschwitz.png', 'Physics degree.png'] 
+var filenames = ['Auschwitz.png', 'Einstein.png', 'Physics degree.png'] 
 // MAKE SURE THE filenames declaration is on the first line
 
 // Code for displaying the last image on index:
